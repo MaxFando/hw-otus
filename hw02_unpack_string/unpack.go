f24458908162fa29de23fcb1fcc2abed20f900cd
mfando <mfando@yandex.ru>
2023-06-18 18:42:16 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	var builder strings.Builder
+	slowPtr := 0
+	fastPtr := 0
+
+	if len(input) == 0 {
+		return "", nil
+	}
+
+	if unicode.IsDigit(rune(input[0])) {
+		return "", ErrInvalidString
+	}
+
+	for fastPtr < len(input) {
+		if input[fastPtr] == '0' && input[slowPtr] == '0' {
+			return "", ErrInvalidString
+		}
+
+		if input[fastPtr] >= '1' && input[fastPtr] <= '9' {
+			builder.WriteString(input[slowPtr : fastPtr-1])
+			builder.WriteString(strings.Repeat(string(input[fastPtr-1]), int(input[fastPtr]-'0')))
+
+			slowPtr = fastPtr + 1
+		} else if input[fastPtr] == '0' {
+			builder.WriteString(input[slowPtr : fastPtr-1])
+
+			slowPtr = fastPtr + 1
+		} else if input[fastPtr] == '\\' {
+			builder.WriteString(input[slowPtr:fastPtr])
+			slowPtr = fastPtr + 1
+			fastPtr++
+		}
+
+		if fastPtr == len(input)-1 {
+			builder.WriteString(input[slowPtr : fastPtr+1])
+		}
+
+		fastPtr++
+	}
+
+	return builder.String(), nil
 }
