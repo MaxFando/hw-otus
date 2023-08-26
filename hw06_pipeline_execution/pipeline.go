8a8ec51beb340635d68df56d75e30c079ee46eeb
mfando <mfando@yandex.ru>
2023-08-26 09:26:19 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	wrapper := func(in In, stage Stage) Out {
+		out := make(Bi)
+		go func() {
+			defer close(out)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					out <- v
+				}
+			}
+		}()
+		return stage(out)
+	}
+
+	for _, stage := range stages {
+		in = wrapper(in, stage)
+	}
+
+	return in
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,16 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty pipeline", func(t *testing.T) {
+		in := make(Bi)
+		close(in)
+
+		result := make([]interface{}, 0)
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Len(t, result, 0)
+	})
 }
