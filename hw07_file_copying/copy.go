6ac13845d1636f8a7db643ce8e9ea6e4733b384a
mfando <mfando@yandex.ru>
2023-09-02 15:04:09 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -10,6 +14,56 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0o666)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrUnsupportedFile
+		}
+		return err
+	}
+	defer fromFile.Close()
+
+	fileInfo, err := fromFile.Stat()
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if fileSize == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	bytesToCopy := fileSize - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	log.Println("bytesToCopy:", bytesToCopy, "offset:", offset, "limit:", limit)
+
+	_, err = fromFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	bar := pb.Start64(bytesToCopy)
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(fromFile)
+	_, err = io.CopyN(toFile, barReader, bytesToCopy)
+	if err != nil && errors.Is(err, io.EOF) {
+		_ = os.Remove(toPath)
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,90 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	inputFilePath  = "./testdata/input.txt"
+	outputFilePath = "output.txt"
+)
+
+func prepareFileForTest(t *testing.T) (*os.File, int64) {
+	t.Helper()
+
+	file, err := os.Open(inputFilePath)
+	require.NoError(t, err)
+
+	fileInfo, err := file.Stat()
+	require.NoError(t, err)
+
+	return file, fileInfo.Size()
+}
+
+func removeTestFile(t *testing.T, filePath string) {
+	t.Helper()
+
+	err := os.Remove(filePath)
+	require.NoError(t, err)
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("file not found", func(t *testing.T) {
+		err := Copy("test.txt", "output.txt", 0, 0)
+		require.Error(t, err)
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		file, fileSize := prepareFileForTest(t)
+
+		err := Copy(file.Name(), "output.txt", fileSize+10, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("unsupported file", func(t *testing.T) {
+		err := Copy("non_existing_file.txt", "output.txt", 0, 0)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("offset 0 limit 0", func(t *testing.T) {
+		file, _ := prepareFileForTest(t)
+
+		err := Copy(file.Name(), outputFilePath, 0, 0)
+		require.NoError(t, err)
+		require.FileExists(t, outputFilePath)
+
+		expected, err := os.ReadFile("testdata/out_offset0_limit0.txt")
+		require.NoError(t, err)
+
+		copied, err := os.ReadFile(outputFilePath)
+		require.NoError(t, err)
+
+		if !bytes.Equal(expected, copied) {
+			t.Errorf("copy file does not math expected")
+		}
+		removeTestFile(t, outputFilePath)
+	})
+
+	t.Run("offset 100 limit 1000", func(t *testing.T) {
+		file, _ := prepareFileForTest(t)
+
+		err := Copy(file.Name(), outputFilePath, 100, 1000)
+		require.NoError(t, err)
+		require.FileExists(t, outputFilePath)
+
+		expected, err := os.ReadFile("testdata/out_offset100_limit1000.txt")
+		require.NoError(t, err)
+
+		copied, err := os.ReadFile(outputFilePath)
+		require.NoError(t, err)
+
+		if !bytes.Equal(expected, copied) {
+			t.Errorf("copy file does not math expected")
+		}
+		removeTestFile(t, outputFilePath)
+	})
 }
