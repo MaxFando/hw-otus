1a6a6a2fa3850b48bfa2a0da7afa80cdf1faa752
mfando <mfando@yandex.ru>
2023-11-02 04:01:01 +0300

    HW8 is completed

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("Использование: go-envdir <директория> <команда>")
+		os.Exit(1)
+	}
+
+	dir := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	env, err := ReadDir(dir)
+	if err != nil {
+		fmt.Println("Ошибка при чтении директории:", err)
+		os.Exit(1)
+	}
+
+	exitCode := RunCmd(cmdArgs, env)
+
+	os.Exit(exitCode)
 }
